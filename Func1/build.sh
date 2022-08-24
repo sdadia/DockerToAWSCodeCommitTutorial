0c4d278587646ec1a8d700be92480f5f5b74758a
sahil <sdadia@cameramatics.com>
2022-08-24 17:31:54 +0100

    Updated

--- Func1/build.sh
+++ Func1/build.sh
@@ -1,5 +1,6 @@
 version=1
+func1ECRRepo_URI="${func1ECRRepo_URI}"
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 637994078207.dkr.ecr.ap-south-1.amazonaws.com
-docker build -t func1:$version .
-docker tag func1:$version 637994078207.dkr.ecr.ap-south-1.amazonaws.com/ecr-repo-stack-func1ecrrepo38a9a079-xg6kwj8qgtwj:$version
+docker build -t func1 .
+docker tag func1:latest $func1ECRRepo_URI:$version
--- Func1/main.py
+++ Func1/main.py
@@ -1,1 +1,4 @@
-print('Hello from func 1')
+def add2(x, y):
+    return x+y
+
+print(f'Hello from func 1 : {add2(1,2)}')
