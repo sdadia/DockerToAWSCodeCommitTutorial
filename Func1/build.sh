8961f607bc9ae5aa088c674e4e958343be06a06a
sahil <sdadia@cameramatics.com>
2022-08-24 17:47:38 +0100

    Updated

--- Func1/build.sh
+++ Func1/build.sh
@@ -1,5 +1,9 @@
-version=1
+version=2
 func1ECRRepo_URI=${1}
+
+
+echo "Building docker image : $func1ECRRepo_URI version : $version"
 
 docker build -t func1 .
 docker tag func1:latest $func1ECRRepo_URI:$version
+docker push $func1ECRRepo_URI:$version
