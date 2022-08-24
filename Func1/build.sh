53f2d93446fcc720e24db6df00edf0389cf90d1b
sahil <sdadia@cameramatics.com>
2022-08-24 17:35:21 +0100

    Updated

--- Func1/build.sh
+++ Func1/build.sh
@@ -1,6 +1,4 @@
 version=1
 func1ECRRepo_URI="${func1ECRRepo_URI}"
 
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 637994078207.dkr.ecr.ap-south-1.amazonaws.com
 docker build -t func1 .
-docker tag func1:latest $func1ECRRepo_URI:$version
