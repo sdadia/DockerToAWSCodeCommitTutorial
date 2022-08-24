6e7053bc1ee96854b8ad9567eb82978a0375ff6c
sahil <sdadia@cameramatics.com>
2022-08-24 17:18:33 +0100

    Using version in shell file

--- Func1/build.sh
+++ Func1/build.sh
@@ -1,4 +1,5 @@
-#aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 637994078207.dkr.ecr.ap-south-1.amazonaws.com
-docker build -t ecr-repo-stack-func1ecrrepo38a9a079-xg6kwj8qgtwj .
-docker tag ecr-repo-stack-func1ecrrepo38a9a079-xg6kwj8qgtwj:latest 637994078207.dkr.ecr.ap-south-1.amazonaws.com/ecr-repo-stack-func1ecrrepo38a9a079-xg6kwj8qgtwj:latest
-#docker push 637994078207.dkr.ecr.ap-south-1.amazonaws.com/ecr-repo-stack-func1ecrrepo38a9a079-xg6kwj8qgtwj:latest
+version=1
+
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 637994078207.dkr.ecr.ap-south-1.amazonaws.com
+docker build -t func1:$version .
+docker tag func1:$version 637994078207.dkr.ecr.ap-south-1.amazonaws.com/ecr-repo-stack-func1ecrrepo38a9a079-xg6kwj8qgtwj:$version
