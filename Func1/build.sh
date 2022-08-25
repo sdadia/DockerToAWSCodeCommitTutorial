2b64b840f2f19e5f6419e91c4ab1db7e96b9ba35
sahil <sdadia@cameramatics.com>
2022-08-25 12:11:36 +0100

    FGake Commit

--- Func1/build.sh
+++ Func1/build.sh
@@ -6,4 +6,5 @@ echo "Building docker image : $func1ECRRepo_URI version : $version"
 
 docker build -t func1 .
 docker tag func1:latest $func1ECRRepo_URI:$version
+
 docker push $func1ECRRepo_URI:$version
